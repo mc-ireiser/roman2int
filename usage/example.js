8c36581c55c1cdff0dd5becc2b51684390b1eb25
Jesús Rodríguez <mc.ireiser@gmail.com>
2020-04-04 23:39:29 -0400

    Usage example

---
+++ usage/example.js
@@ -0,0 +1,40 @@
+const roman2int = require("../dist/index.min");
+
+const romanNumbers = [
+  "",
+  "XLIX",
+  "MDCCXCVIII",
+  "MCMLII",
+  "MMDCCCLXXXIII",
+  "MDCCCLXXXII",
+  "MMDCCCLXIII",
+  "XD",
+  "MMMDLXI",
+  "MMMDCCCLXVIII",
+  "MMDCCCLXIX",
+  "MDCCCLXVI",
+  "iii",
+  "MMDCXXXIX",
+  "MMMCLXVII",
+  "MMMDCCXLVII",
+  "0"
+];
+
+let numbersMap = [];
+
+romanNumbers.forEach((romanNumber, index) => {
+  const intNumber = roman2int(romanNumber);
+
+  typeof intNumber === "number" ?
+    numbersMap.push({
+      roman: romanNumber,
+      arabic: intNumber
+    }) :
+    console.log(
+      `- Pass# ${index},`,
+      `Roman numeral: ${romanNumber},`,
+      `roman2int error: ${intNumber}`
+    );
+});
+
+console.log("- numbersMap", numbersMap);

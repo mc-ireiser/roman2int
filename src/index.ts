1c9c0f9f445736b136b7c7db94e19f37fab61bdd
Jesús Rodríguez <mc.ireiser@gmail.com>
2020-04-20 16:04:51 -0400

    📦 NEW: Typescript

---
+++ src/index.ts
@@ -0,0 +1,65 @@
+const regex: RegExp = /^M{0,3}(?:D?C{0,3}|C[MD])(?:L?X{0,3}|X[CL])(?:V?I{0,3}|I[XV])$/;
+
+let romeNums: { [index: string]: number } = {
+  I: 1,
+  IV: 4,
+  V: 5,
+  IX: 9,
+  X: 10,
+  XL: 40,
+  L: 50,
+  XC: 90,
+  C: 100,
+  CD: 400,
+  D: 500,
+  CM: 900,
+  M: 1000,
+};
+
+/**
+ * Receive a string with the Roman numeral for processing.
+ * @param  {string} value - Roman numeral.
+ * @returns {number | string} returnValue - Number | "String is required" | "Invalid or unsupported roman numeral"
+ */
+function roman2int(value: string) {
+  if (!value || typeof value !== 'string') {
+    return 'String is required';
+  }
+
+  const imputString = value.trim();
+  const isValidRomanNum = regex.test(imputString);
+
+  return response(isValidRomanNum, value);
+}
+
+/**
+ * @param  {boolean} isValidRomanNum - Valid roman numeral.
+ * @param  {string} value - Roman numeral.
+ * @returns {number | string} returnValue - Number | "Invalid or unsupported roman numeral"
+ */
+function response(isValidRomanNum: boolean, value: string) {
+  const romeNum = value.trim();
+
+  if (isValidRomanNum) {
+    return calculateInteger(romeNum);
+  } else {
+    return 'Invalid or unsupported roman numeral';
+  }
+}
+
+/**
+ * @param  {string} romeNum - Roman numeral.
+ * @returns {number} returnValue - Number
+ */
+function calculateInteger(romeNum: string) {
+  let result: number = 0;
+
+  romeNum.replace(/[MDLV]|C[MD]?|X[CL]?|I[XV]?/g, function (item: string) {
+    result += romeNums[item.toString()];
+    return 'true';
+  });
+
+  return result;
+}
+
+module.exports = roman2int;
